95a75f5cb775cab34b77f4fb71ab14d62ac09683
yorimek <yorimek@f4r2s15.paris.42.school>
2026-06-12 14:55:39 +0200

    update: ex00 fixed

--- ex00/src/Fixed.cpp
+++ ex00/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: yorimek <yorimek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 15:01:57 by yorimek           #+#    #+#             */
-/*   Updated: 2026/06/12 12:45:41 by yorimek          ###   ########.fr       */
+/*   Updated: 2026/06/12 14:55:28 by yorimek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 const int Fixed::_number_of_fractional_bits = 8;
 
-Fixed::Fixed(): _fixed_point_number(0)
+Fixed::Fixed(): _raw_bits(0)
 {
 	std::cout << "Default constructor called" << std::endl;
 	return ;
@@ -36,18 +36,18 @@ Fixed::Fixed(Fixed const &other)
 Fixed	&Fixed::operator=(Fixed const &other)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
-	this->_fixed_point_number = other.getRawBits();
+	this->_raw_bits = other.getRawBits();
 	return (*this);
 }
 
 int		Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called" << std::endl;
-	return (_fixed_point_number);
+	return (_raw_bits);
 }
 
 void	Fixed::setRawBits(int const raw)
 {	
-	this->_fixed_point_number = raw;
+	this->_raw_bits = raw;
 	return ;
 }
